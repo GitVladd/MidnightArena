6a77dd0d4ebf229c8cc1272290326ef2eb069c14
Vladlavv <vovsiienko@myseneca.ca>
2023-09-10 17:48:43 +0300

    Added EffectSystemComponent

---
+++ Source/MidnightArena/Effect/Effect.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Effect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MIDNIGHTARENA_API UEffect : public UObject
+{
+	GENERATED_BODY()
+	
+};
